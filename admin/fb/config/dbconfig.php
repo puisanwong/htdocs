f512c0a4fc22527ef629b646e441066559f2ac0f
puisanwong <p.s.wong124@gmail.com>
2012-12-31 21:08:32 +0800

    Update admin/fb/config/dbconfig.php

--- admin/fb/config/dbconfig.php
+++ admin/fb/config/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'instance35319.db.xeround.com:3874');
+define('DB_USERNAME', 'comp320');
+define('DB_PASSWORD', 'comp320');
 define('DB_DATABASE', 'phpwebco_shop');
 $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die(mysql_error());
 $database = mysql_select_db(DB_DATABASE) or die(mysql_error());
