505c0a0a06f611d6c01c9b369eb5e84b538759b1
puisanwong <p.s.wong124@gmail.com>
2013-01-06 09:39:20 -0800

    Update autocomplete/autoCompleteMain.php

--- autocomplete/autoCompleteMain.php
+++ autocomplete/autoCompleteMain.php
@@ -8,7 +8,9 @@ $db_name='phpwebco_shop'; // Database name
 
 	$con = mysql_connect($host,$username,$password)   or die(mysql_error());
 	mysql_select_db($db_name, $con)  or die(mysql_error());
-
+	
+$q = strtolower($_GET["q"]);
+if (!$q) return;
 
 $sql = "SELECT pd_name,cat_id,pd_id FROM  tbl_product where pd_name LIKE '%$q%'";
 $rsd = mysql_query($sql);
