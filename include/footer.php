92c5e5a74f86d85a4f132e34ca71709669c8b2df
puisanwong <p.s.wong124@gmail.com>
2013-01-06 20:22:10 +0800

    Update include/footer.php

--- include/footer.php
+++ include/footer.php
@@ -7,8 +7,8 @@ if (!defined('WEB_ROOT')) {
 <table width="100%" border="0" cellspacing="0" cellpadding="10">
  <tr>
   <td align="center">
-   <p>&copy; <?php echo '2012 - ' . date('Y'); ?> <?php echo $shopConfig['name']; ?></p>
-   <p>Address : <?php echo $shopConfig['address']; ?><br>
+   <p>&copy; <?php echo '2012 - ' . date('Y'); ?> <?php echo $shopConfig['name']; ?><br>
+    Address : <?php echo $shopConfig['address']; ?><br>
     Phone : <?php echo $shopConfig['phone']; ?><br>
     Email : <a href="mailto:<?php echo $shopConfig['email']; ?>"><?php echo $shopConfig['email']; ?></a></p>
    <p><br>
